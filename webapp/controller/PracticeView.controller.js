421d082ae492c3cd0eb0d647e54236d7ff9dd69a
sree vedavyasa rangarajasree <svrangarajasree05@gmail.com>
2024-11-18 11:10:28 +0000

    Handle Search in F4 Dialog

--- webapp/controller/PracticeView.controller.js
+++ webapp/controller/PracticeView.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], (Controller,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("practicefiori.controller.PracticeView", {
@@ -56,6 +58,23 @@ sap.ui.define([
 			var s = this.getView().byId("_IDGenInput").getValue();
            s  = oSelectedItem.getTitle();
 			oCreateModel.refresh();
+        },
+        handleSearch (oEvent){
+          var sValue = oEvent.getParameter("value");
+         
+          if (sValue) {
+            var oFilter = new Filter("name", FilterOperator.Contains, sValue);
+            var oFilter1 = new Filter("value", FilterOperator.Contains, sValue);
+            var oBinding = oEvent.getParameter("itemsBinding");
+            var oFinalFilter = new Filter({
+                filters: [oFilter, oFilter1],
+                and: false
+            });
+            oBinding.filter([oFilter]);
+        } else {
+            oEvent.getSource().getBinding("items").filter([]);
+        }
+
         }
     });
 });
